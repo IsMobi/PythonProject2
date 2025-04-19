1addc12db9e631a10590708e1a25e9bef27ebb9f
Jinwoo Chang <jinwoo990419@gmail.com>
2025-04-19 14:11:59 +0900

    Update in local A

--- main.py
+++ main.py
@@ -13,4 +13,6 @@ def print_hi(name):
 if __name__ == '__main__':
     print_hi('Hello World 1 in team Leader server')
 
+    print_hi('Update in local A')
+
 # https://www.jetbrains.com/help/pycharm/에서 PyCharm 도움말 참조
